be75667056c62a2d546abf876ae3a2162ec4a6a6
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-12 18:02:43 +0300

    learned and implemented the keyup event on a form

--- ch_7_Forms/form.js
+++ ch_7_Forms/form.js
@@ -1,18 +1,27 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     // basic form validation
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/
 
     if (usernamePattern.test(username)) {
-        feedback.classList.add('success');
+        // feedback.classList.add('success');
         feedback.textContent = "The username is valid";
     } else {
-        feedback.classList.add('error');
+        // feedback.classList.add('error');
         feedback.textContent = "The username should be alphabetic letters and between 6 to 12 characters long";
     }
 
+});
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+    if (usernamePattern.test(e.target.value)) {
+        form.username.setAttribute('class', 'success');
+    } else {
+        form.username.setAttribute('class', 'error');
+    }
 });
