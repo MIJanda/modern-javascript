ceb6be2f2d870390017aa879f31fa641072b9278
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-15 13:51:08 +0300

    add and deletion features implemented

---
+++ ch_10_Todo_Mini_Project/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const addForm = document.querySelector('.add');
+const todos = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+        todos.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+            `;
+};
+
+
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const todo = addForm.add.value.trim();
+
+    
+    if (todo.length) {
+        generateTemplate(todo);
+        addForm.reset()
+    }
+});
+
+todos.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
