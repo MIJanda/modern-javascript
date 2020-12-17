bb15185a9d7bcea3f1dc626ba5c552c2e9a1d14b
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-17 16:34:38 +0300

    implemented a callback function to fetch data and illustrated the asynchronous nature of such a request

--- ch_12_Async_JS/http_requests.js
+++ ch_12_Async_JS/http_requests.js
@@ -1,12 +1,32 @@
-const request = new XMLHttpRequest();
+const getTodos = callback => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 400) {
-        console.log(request.responseText);
-    } else if (request.readyState === 4) {
-        console.log('Could not fetch the data.');
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 400) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            callback('Could not fetch the data.', undefined);
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+console.log(1);
+console.log(2);
+
+// Asynchronous code
+getTodos((err, data) => {
+    console.log('Callback fired!');
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
+
+
