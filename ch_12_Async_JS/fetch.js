99609a9b00c179c9131c685ad36996a1e9451517
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-20 15:08:43 +0300

    implemented a custom error message

--- ch_12_Async_JS/fetch.js
+++ ch_12_Async_JS/fetch.js
@@ -1,23 +1,16 @@
-// fetch API
-// fetch('todos/luigi.json')
-//     .then(response => {
-//         console.log('resolved:', response);
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log('rejected:', err);
-//     });
-
 // async & await
 const getTodos = async () => {
     const response = await fetch('todos/luigi.json');
+
+    if (response.status !== 200) {
+        throw new Error('Cannot fetch the resource');
+    }
+
     const data = await response.json();
 
     return data;
 };
 
 getTodos()
-    .then(todos =>  console.log(todos));
+    .then(todos =>  console.log(todos))
+    .catch(err => console.log(err.message));
