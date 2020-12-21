15b7e6423d73db9ed17c15659965b73bc508ba06
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-21 11:59:03 +0300

    implemented method chaining to the User class

--- ch_15_OOP/classes.js
+++ ch_15_OOP/classes.js
@@ -2,24 +2,28 @@ class User {
     constructor (username, email) {
         this.username = username;
         this.email = email;
+        this.score = 0;
     }
 
     login () {
         console.log(`${this.username} just logged in...`);
+        return this;
     }
 
     logout () {
         console.log(`${this.username} just logged out...`);
+        return this;
+    }
+
+    incScore () {
+        this.score++;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
 const userOne = new User('mario', 'mario@classes.co.ug');
-userOne.login();
+userOne.login().incScore().incScore().logout();
 
 const userTwo = new User('luigi', 'luigi@classes.co.ug');
-userTwo.login();
-
-console.log(userOne, userTwo);
-
-userOne.logout();
-userTwo.logout();
+userTwo.login().incScore().logout();
