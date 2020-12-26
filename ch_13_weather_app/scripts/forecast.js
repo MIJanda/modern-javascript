a8657905a3e6f7f2b3ee17be7c4fe4ce871199b0
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-26 09:52:22 +0300

    implemented interaction with Accuweather API

--- ch_13_weather_app/scripts/forecast.js
+++ ch_13_weather_app/scripts/forecast.js
@@ -1,1 +1,29 @@
 const key = 'aGZKsITGJ4ypXLr8UfOsvFVDeeQa3ne7';
+
+// get current weather
+const getWeather = async id => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+// get city information
+const getCity = async city => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+getCity('Kampala').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+})
+.catch(err => console.log(err));
