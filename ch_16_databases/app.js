2eae868f3cd1be9a10f13d38f920865daaacd027
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-28 14:04:37 +0300

    implemented realtime listeners to the app

--- ch_16_databases/app.js
+++ ch_16_databases/app.js
@@ -1,26 +1,39 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = recipe => {
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate();
     let html = `
-        <li>
+        <li data-id="${id}">
             <div> ${recipe.title} </div>
             <div>${time}</div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
         </li>
     `;
 
     list.innerHTML += html; 
 };
 
+const deleteRecipe = id => {
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+        if (recipe.getAttribute('data-id') === id) {
+            recipe.remove();
+        }
+    });
+};
+
 // get recipes
-db.collection('recipes').get()
-    .then(snapshot => {
-        snapshot.docs.forEach(doc => {
-            addRecipe(doc.data());
-        });
-    })
-    .catch(err => console.log(err));
+db.collection('recipes').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+        if (change.type === 'added') {
+            addRecipe(doc.data(), doc.id);
+        } else if (change.type === 'removed') {
+            deleteRecipe(doc.id);
+        }
+    });
+});
 
 // add recipes
 form.addEventListener('submit', e => {
@@ -35,6 +48,22 @@ form.addEventListener('submit', e => {
     db.collection('recipes').add(recipe)
         .then(() => {console.log('recipe added')})
         .catch(err => console.log(err));
+
+    form.reset();
+});
+
+// deleting recipes
+list.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON') {
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete()
+            .then(() => {
+                console.log('recipe deleted');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 });
 
 
